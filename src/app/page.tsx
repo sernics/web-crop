2b9dfc8df88cca9653ff86cd10ee6561cdc184ff
sernics <sernics@pachi>
2025-01-09 13:58:18 +0000

    Falta hacer que la api devuelva si esta sana o no e imprimirlo en la web

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 
 export default function Page() {
   const [image, setImage] = useState<string | null>(null);
+  const [message, setMessage] = useState("");
 
   const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
@@ -16,10 +17,23 @@ export default function Page() {
     }
   };
 
-  const handleSaveImage = () => {
+  const handleSaveImage = async () => {
     if (image) {
-      console.log('Imagen guardada:', image); // Aquí podrías enviar la imagen a tu servidor.
-      alert('Imagen guardada correctamente.');
+      const base64 = image.split(',')[1]; // Elimina el prefijo de la URI base64
+      setMessage("Loading...");
+      try {
+        const response = await fetch('http://127.0.0.1:5000/predict', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ image: base64 }),
+        });    
+        const data = await response.json();
+        console.log('Respuesta de la API:', data);
+      } catch (error) {
+        console.error('Error al enviar la imagen:', error);
+      }
     } else {
       alert('No se ha seleccionado ninguna imagen.');
     }
@@ -28,7 +42,7 @@ export default function Page() {
   return (
     <div className="flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8 py-6 space-y-8">
       <Section title="Crop Recognition Of Plants Diseases" className="w-3/4 border-2 border-[#697565] p-6 rounded-lg text-center" />
-      <SmallSection title="Suba una imagen de la planta que desee evaluar, y el sistema indicará si la planta tiene alguna enfermedad." className="w-3/4 max-w-full border-2 border-[#697565] p-6 rounded-lg text-center" />
+      <SmallSection title="Suba  una imagen de la planta que desee evaluar, y el sistema indicará si la planta tiene alguna enfermedad." className="w-3/4 max-w-full border-2 border-[#697565] p-6 rounded-lg text-center" />
 
       <div className="w-3/4 max-w-full border-2 border-[#697565] p-6 rounded-lg">
         <div className="flex flex-col items-center space-y-4">
