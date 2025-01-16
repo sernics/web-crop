9115c4538702c383d3a5f583cc48945cd50fd952
Sergio Nicolas Segui <sergionicolassegui@MacBook-de-Sergio.local>
2025-01-16 10:48:27 +0000

    Finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,6 +20,7 @@ export default function Page() {
   const handleSaveImage = async () => {
     if (image) {
       const base64 = image.split(',')[1]; // Elimina el prefijo de la URI base64
+      console.log(base64);
       setMessage("Loading...");
       try {
         const response = await fetch('http://127.0.0.1:5000/predict', {
@@ -41,7 +42,7 @@ export default function Page() {
 
   return (
     <div className="flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8 py-6 space-y-8">
-      <Section title="Crop Recognition Of Plants Diseases" className="w-3/4 border-2 border-[#697565] p-6 rounded-lg text-center" />
+      <Section title="Computerized Recognition Of Plants diseases" className="w-3/4 border-2 border-[#697565] p-6 rounded-lg text-center" />
       <SmallSection title="Suba  una imagen de la planta que desee evaluar, y el sistema indicará si la planta tiene alguna enfermedad." className="w-3/4 max-w-full border-2 border-[#697565] p-6 rounded-lg text-center" />
 
       <div className="w-3/4 max-w-full border-2 border-[#697565] p-6 rounded-lg">
@@ -115,7 +116,7 @@ function SaveButton({ onClick, message }: { onClick: () => void; message: string
         onClick={onClick}
         className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
       >
-        Guardar Imagen
+        Subir Imagen
       </button>
       <span>{message}</span>
     </div>
